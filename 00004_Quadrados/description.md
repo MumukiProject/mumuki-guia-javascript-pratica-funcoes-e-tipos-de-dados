88da0784acb757142fb5ab8283ec2642df2c9718
nadia@mumuki.org <nadia@mumuki.org>
2018-01-30 11:05:41 -0500

    Mumuki Export on 2018-01-30 11:05:41 -0500

---
+++ 00004_Quadrados/description.md
@@ -0,0 +1,6 @@
+E agora é a vez dos quadrados; queremos saber:
+
+* `perimetroQuadrado`: dado um lado, queremos saber quanto mede o perímetro.
+* `areaQuadrado`: dado um lado, queremos saber qual é a área.
+
+> Desenvolva as funções `perimetroQuadrado` e `areaQuadrado`.
---
+++ 00004_Quadrados/test..js
@@ -0,0 +1,5 @@
+it("perimetroQuadrado(10) es 40", () => { assert.equal(perimetroQuadrado(10), 40) });
+it("perimetroQuadrado(20) es 80", () => { assert.equal(perimetroQuadrado(20), 80) });
+
+it("areaQuadrado(10) es 100", () => { assert.equal(areaQuadrado(10), 100) });
+it("areaQuadrado(5) es 25", () => { assert.equal(areaQuadrado(5), 25) });
