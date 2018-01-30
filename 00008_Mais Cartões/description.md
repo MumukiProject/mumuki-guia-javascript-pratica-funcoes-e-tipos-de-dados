b2fa4bcfb9979c16def72723e54990c24f92df70
nadia@mumuki.org <nadia@mumuki.org>
2018-01-30 13:17:49 -0500

    Mumuki Export on 2018-01-30 13:17:49 -0500

--- 00008_Mais Cartões/description.md
+++ 00008_Mais Cartões/description.md
@@ -6,11 +6,11 @@ Ah, mas não tão rápido! Às vezes, no nosso cartão :name_badge: , só querem
 * O nome e sobrenome, como até agora.
 * Um booleano que nos diga se queremos um cartão curto com apenas título e sobrenome, ou um longo, como até agora.
 
-> Modifique a função `escreverCartoes`, de modo que se comporte como é descrito acima. Exemplo:
+> Modifique a função `escreverCartao`, de modo que se comporte como é descrito acima. Exemplo:
 
 > ``` javascript
-> ム escreverCartoes ("Lic.", "Eduardo", "Peralta", true)
+> ム escreverCartao ("Lic.", "Eduardo", "Peralta", true)
 > "Lic. Peralta"
-> ム escreverCartoes ("Ing.", "Julia", "Velázquez", false)
+> ム escreverCartao ("Ing.", "Julia", "Velázquez", false)
 > "Ing. Julia Velázquez"
 > ```
--- 00008_Mais Cartões/test..js
+++ 00008_Mais Cartões/test..js
@@ -1,12 +1,12 @@
-it("escreverCartoes(\"Dra.\", \"Ana\", \"Pérez\", false), é \"Dra. Ana Pérez\"", () => {
-  assert.equal(escreverCartoes("Dra.", "Ana", "Pérez", false), "Dra. Ana Pérez")
+it("escreverCartao(\"Dra.\", \"Ana\", \"Pérez\", false), é \"Dra. Ana Pérez\"", () => {
+  assert.equal(escreverCartao("Dra.", "Ana", "Pérez", false), "Dra. Ana Pérez")
 })
-it("escribirCaescreverCartoesrtelito(\"Dr.\", \"Julio\", \"Gelman\", false), é \"Dr. Julio Gelman\"", () => {
-  assert.equal(escreverCartoes("Dr.", "Julio", "Gelman", false), "Dr. Julio Gelman")
+it("escreverCartao(\"Dr.\", \"Julio\", \"Gelman\", false), é \"Dr. Julio Gelman\"", () => {
+  assert.equal(escreverCartao("Dr.", "Julio", "Gelman", false), "Dr. Julio Gelman")
 })
-it("escreverCartoes(\"Dra.\", \"Ana\", \"Pérez\", true), é \"Dra. Pérez\"", () => {
-  assert.equal(escreverCartoes("Dra.", "Ana", "Pérez", true), "Dra. Pérez")
+it("escreverCartao(\"Dra.\", \"Ana\", \"Pérez\", true), é \"Dra. Pérez\"", () => {
+  assert.equal(escreverCartao("Dra.", "Ana", "Pérez", true), "Dra. Pérez")
 })
-it("escreverCartoes(\"Dr.\", \"Julio\", \"Gelman\", true), é \"Dr. Gelman\"", () => {
-  assert.equal(escreverCartoes("Dr.", "Julio", "Gelman", true), "Dr. Gelman")
+it("escreverCartao(\"Dr.\", \"Julio\", \"Gelman\", true), é \"Dr. Gelman\"", () => {
+  assert.equal(escreverCartao("Dr.", "Julio", "Gelman", true), "Dr. Gelman")
 })
