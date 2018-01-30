7cc997a2ab9c013fd53ff0f675289dd98e221896
nadia@mumuki.org <nadia@mumuki.org>
2018-01-30 12:43:45 -0500

    Mumuki Export on 2018-01-30 12:43:45 -0500

--- 00010_Cara ou Coroa/test..js
+++ 00010_Cara ou Coroa/test..js
@@ -1,14 +1,14 @@
 describe("decisaoComMoeda", () => {
-  it("decisaoComMoeda(\"cara\", \"coxinha\", \"empanadas\") es \"pizzas\"", () => {
-    assert.equal(decisaoComMoeda("cara", "coxinha", "empanadas"), "pizzas")
+  it("decisaoComMoeda(\"cara\", \"coxinha\", \"empadinha\") é \"coxinha\"", () => {
+    assert.equal(decisaoComMoeda("cara", "coxinha", "empadinha"), "coxinha")
   })
-  it("decisaoComMoeda(\"cara\", \"asado\", \"empanadas\") es \"asado\"", () => {
-    assert.equal(decisaoComMoeda("cara", "asado", "empanadas"), "asado")
+  it("decisaoComMoeda(\"cara\", \"churrasqueira\", \"empadinha\") é \"churrasqueira\"", () => {
+    assert.equal(decisaoComMoeda("cara", "churrasqueira", "empadinha"), "churrasqueira")
   })
-  it("decisaoComMoeda(\"coroa\", \"coxinha\", \"empanadas\") es \"empanadas\"", () => {
-    assert.equal(decisaoComMoeda("coroa", "coxinha", "empanadas"), "empanadas")
+  it("decisaoComMoeda(\"coroa\", \"coxinha\", \"empadinha\") é \"empadinha\"", () => {
+    assert.equal(decisaoComMoeda("coroa", "coxinha", "empadinha"), "empadinha")
   })
-  it("decisaoComMoeda(\"coroa\", \"coxinha\", \"pastas\") es \"pastas\"", () => {
-    assert.equal(decisaoComMoeda("coroa", "coxinha", "pastas"), "pastas")
+  it("decisaoComMoeda(\"coroa\", \"coxinha\", \"macarrão\") é \"macarrão\"", () => {
+    assert.equal(decisaoComMoeda("coroa", "coxinha", "macarrão"), "macarrão")
   })
 })
