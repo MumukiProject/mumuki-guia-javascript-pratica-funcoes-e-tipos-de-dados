4d808dcb29b1c6a09a1e9702c3516d0139dbd141
nadia@mumuki.org <nadia@mumuki.org>
2018-01-31 08:13:31 -0500

    Mumuki Export on 2018-01-31 08:13:31 -0500

---
+++ extra.js
@@ -0,0 +1,31 @@
+function longitude(secuencia) {
+  return secuencia.length; 
+}
+
+function rango(s, e) {
+  let r = [];
+  for (let i = s; i <= e; i++) {
+     r.push(i)
+  }
+  return r;
+}
+
+function imprimir(s) {
+  console.log(s)
+}
+
+function colocar(a, e) {
+   array.push(e);
+}
+
+function absoluto(x) {
+  return Math.abs(x)
+}
+
+function convertirEmMaiuscula(x) {
+  return x.toUpperCase()
+}
+
+function comecaCom(x, y) {
+  return x.startsWith(x, y);
+}
